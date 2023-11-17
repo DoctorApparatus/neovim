8ab52ee154db6c57851aac7f7ce0d660f11ffbe7
Kilian Ischtschuk <kilian.ischtschuk@apparatus.network>
2023-11-17 09:41:32 +0100

    feat: better spacing in cmp (needed for future styling changes)

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -66,7 +66,7 @@ return {
                     kind.kind, "%s",
                     { trimempty = true }
                 )
-                kind.kind = " " .. (strings[1] or "") .. " "
+                kind.kind = " " .. (strings[1] or " ") .. "  "
                 kind.menu = "    (" .. (strings[2] or "") .. ")"
 
                 return kind
