26938888321c74dbeb69fe943b28667db38da40e
DoctorApparatus <doctor.apparatus@gmail.com>
2024-02-26 10:09:17 +0100

    refactor: 💡 init.lua cleanup

--- init.lua
+++ init.lua
@@ -1,22 +1,15 @@
--- Enable true color
-vim.opt.termguicolors = true
-vim.cmd([[set termguicolors]])
-
 require("plugins/main")
 require("autocommands/main")
 
-vim.cmd([[set tabstop=4]])
-vim.cmd([[set shiftwidth=4]])
-vim.cmd([[set so=999]])
-
-vim.o.ls = 2
-vim.o.ch = 0
-vim.o.completeopt = "menuone,noinsert,noselect"
+vim.opt.termguicolors = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.scrolloff = 999
+vim.opt.laststatus = 2
+vim.opt.cmdheight = 0
 vim.opt.backup = false
 vim.opt.clipboard = "unnamedplus"
-vim.opt.cmdheight = 2
 vim.opt.completeopt = { "menuone", "noselect" }
-vim.conceallevel = 0
 vim.opt.fileencoding = "utf-8"
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
@@ -40,28 +33,53 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.numberwidth = 4
 vim.opt.signcolumn = "yes"
-vim.opt.wrap = false
 vim.scrolloff = 8
 vim.sidescrolloff = 8
 vim.opt.shortmess:append("c")
+vim.opt.conceallevel = 2
 
 -- KEYMAPS
 local keymap = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
--- local term_opts = { silent = true }
-
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Easier normal mode
 keymap("i", "jk", "<ESC>", default_opts)
 keymap("t", "jk", "<C-\\><C-n>", default_opts)
-
--- vim.keymap.set("n", "bd", function()
---     vim.cmd("NeoTreeClose")
---     vim.cmd("bd")
---     vim.cmd("NeoTreeReveal")
--- end, default_opts)
 
 -- Increment / Decrement
 vim.keymap.set("n", "+", "<C-a>")
 vim.keymap.set("n", "-", "<C-x>")
+
+-- Close current buffer and go to previous
+vim.keymap.set("n", "<leader>bd", "<cmd>bp|bd #<cr>", { desc = "close current buffer" })
+
+local vim = vim
+local opt = vim.opt
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+local api = vim.api
+local M = {}
+
+-- function to create a list of commands and convert them to autocommands
+-------- This function is taken from https://github.com/norcalli/nvim_utils
+function M.nvim_create_augroups(definitions)
+	for group_name, definition in pairs(definitions) do
+		api.nvim_command("augroup " .. group_name)
+		api.nvim_command("autocmd!")
+		for _, def in ipairs(definition) do
+			local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
+			api.nvim_command(command)
+		end
+		api.nvim_command("augroup END")
+	end
+end
+local autoCommands = {
+	-- other autocommands
+	open_folds = {
+		{ "BufReadPost,FileReadPost", "*", "normal zR" },
+	},
+}
+M.nvim_create_augroups(autoCommands)
